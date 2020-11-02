884a60e24779973bc7b240ea9e3391f5ce0b0d2a
Willian <williamhernandez450@gmail.com>
2020-11-01 19:42:19 -0600

    Subida del proyecto

---
+++ app/src/main/java/com/willianhdz/crudmysql/Setting_VAR.java
@@ -0,0 +1,24 @@
+package com.willianhdz.crudmysql;
+
+public class Setting_VAR {
+    public static final String TAG = "InicioActivity";
+
+    public static final String mensaje = "";
+
+   // public static final String URL_PRUEBA = "http://"
+    //Para pruebas en local
+    /*
+    public static final String URL_guardar_categoria = "http://192.168.108.2/service2020/guardar_categorias.php";
+    public static final String URL_buscar_categoria_ID = "http://192.168.108.2/service2020/buscar_categorias_ID.php";
+    //url para lista en spinner de la llave foranea
+    public static final String URL_consultaAllCategorias = "http://192.168.108.2/service2020/buscarCategorias.php";
+    public static final String URL_registrar_productos = "http://192.168.108.2/service2020/guardar_productos.php";
+
+
+     */
+   //Para conexion remota
+    public static final String URL_guardar_categoria = "https://androidhernandez.000webhostapp.com/webservice/guardar_categorias.php";
+    public static final String URL_buscar_categoria_ID = "https://androidhernandez.000webhostapp.com/webservice/buscar_categorias_ID.php";
+    public static final String URL_consultaAllCategorias = "https://androidhernandez.000webhostapp.com/webservice/buscar_categoria.php";
+    public static final String URL_registrar_productos = "https://androidhernandez.000webhostapp.com/webservice/guardar_productos.php";
+}
